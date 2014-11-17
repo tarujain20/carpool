4aa8438d3ab885e5ae0ad3934aeb21461b37c202
Taru Jain <tarujain@Tarus-MacBook-Air.local>
2014-11-17 15:00:53 -0800

    User can create ride request or offer a ride.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,10 +4,25 @@ class RidesController < ApplicationController
   def index
     find_params
     @rides = []
-    if @origin && @destination
+    if @origin.present? && @destination.present?
       @rides = Ride.search_by_origin_destination(@origin, @destination)
-    elsif @destination
+    elsif @destination.present?
       @rides = Ride.search_by_destination(@destination)
+    end
+  end
+
+  def new
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = new_ride
+    @ride.date = 1.day.from_now.to_date
+    if @ride.save
+      redirect_to rides_url(:ride => {:destination => @ride.destination}), :notice => "Successfully added ride."
+    else
+      flash.now[:alert] = "#{@ride.errors.first}"
+      render :action => :new
     end
   end
 
@@ -16,5 +31,18 @@ class RidesController < ApplicationController
   def find_params
     @origin = params[:ride][:origin]
     @destination = params[:ride][:destination]
+  end
+
+  def ride_params
+    params.require(:ride).permit(:type, :origin, :destination, :total_seat, :date)
+  end
+
+  def new_ride
+    ride_type = params[:ride][:type]
+    if ride_type == "RideOffer"
+      @ride = RideOffer.new(ride_params)
+    else
+      @ride = RideRequest.new(ride_params)
+    end
   end
 end
--- spec/controllers/rides_controller_spec.rb
+++ spec/controllers/rides_controller_spec.rb
@@ -12,7 +12,7 @@ describe RidesController, :type => :controller do
   describe "index" do
     context "no date entered" do
       it "shows all available rides in future" do
-        get :index, :ride => { :origin => "San Jose", :destination => "North Fork" }
+        get :index, :ride => {:origin => "San Jose", :destination => "North Fork"}
 
         expect(response).to be_success
         expect(assigns(:rides).to_a).to eq([@ride1])
@@ -22,11 +22,46 @@ describe RidesController, :type => :controller do
 
     context "no origin entered" do
       it "shows all available rides in future going to the destination" do
-        get :index, :ride => { :destination => "North Fork" }
+        get :index, :ride => {:destination => "North Fork"}
 
         expect(response).to be_success
         expect(assigns(:rides).to_a).to eq([@ride1, @ride2])
       end
+    end
+  end
+
+  describe "#new" do
+    it "show the form" do
+      get :new
+      expect(response).to be_success
+      expect(assigns[:ride]).to be_new_record
+    end
+  end
+
+  describe "create" do
+    context "Request a Ride" do
+      it "creates new RideRequest" do
+        expect do
+          post :create, :ride => {:type => "RideRequest", :origin => "San Jose", :destination => "Intel HQ", :total_seat => 1, :date => 2.day.from_now.to_date}
+        end.to change(RideRequest, :count).by(1)
+        expect(response).to redirect_to(rides_url(:ride => {:destination => "Intel HQ"}))
+      end
+    end
+
+    context "Offer a Ride" do
+      it "creates new RideOffer" do
+        expect do
+          post :create, :ride => {:type => "RideOffer", :origin => "San Jose", :destination => "Intel HQ", :total_seat => 1, :date => 2.day.from_now.to_date}
+        end.to change(RideOffer, :count).by(1)
+        expect(response).to redirect_to(rides_url(:ride => {:destination => "Intel HQ"}))
+      end
+    end
+
+    it "show form with errors" do
+      expect do
+        post :create, :ride => {:type => "RideRequest", :origin => "", :destination => "Intel HQ", :total_seat => 1, :date => 2.day.from_now.to_date}
+      end.not_to change(Ride, :count)
+      expect(response).to render_template(:new)
     end
   end
 end
