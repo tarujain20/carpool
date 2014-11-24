8819ce1bfadcf53c9ba8bb9f935bbef83328bcb7
Taru Jain <tarujain@Tarus-MacBook-Air.local>
2014-11-23 17:13:37 -0800

    business name and email is required to create ride request or offer a ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,7 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
 
-  validates :origin, :destination, :total_seat, :user, :origin_address, :destination_address, presence: true
+  validates :origin, :destination, :total_seat, :user, :business_email, :business_name, :origin_address, :destination_address, presence: true
 
   scope :search_by_origin_destination, lambda { |origin, destination| where("origin = ? AND destination = ?", origin, destination).order("destination") }
   scope :search_by_destination, lambda { |destination| where("destination = ?", destination).order("destination") }
